a9300125f001c00d3f63064e2b91e8603a057853
Franco Gallo <gallofrancogabriel@gmail.com>
2024-02-15 20:17:43 +0100

    Loading screen in Excel component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { getFirestoreApp } from "./db/dbFirebase";
 import Excel from "./components/Excel";
+import { useState, useEffect } from "react";
 
 getFirestoreApp()
 // Function that brings the firebase db to the entire app (do not touch).
 
 function App() {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+  }, [])
+  // Small timeout to give time to the loading screen
+
   return (
     <div>
       <h1>¡Hola Mundo!</h1>
 
-      <Excel />
+      {
+        loading
+        ? <h1>Cargando..</h1>
+        : <Excel />
+      }
     </div>
   );
 }
